d97c397b9b3869cf238b074c5b11a3d29fce4504
LetiMalaga <leticiamalagutti@yahoo.com.br>
2024-06-17 20:28:58 -0300

    ajuste livro.js

--- livraria/database.js
+++ livraria/database.js
@@ -18,14 +18,22 @@ db.serialize(() => {
 
 // Inicialização do banco de dados com dados do JSON
 const fs = require('fs');
-const Livro = require('./models/livro');
+const Livro = require('./models/livro'); // Importação da classe Livro
 
 const livrosJson = JSON.parse(fs.readFileSync('livros.json', 'utf8')).books;
 
 livrosJson.forEach((livro) => {
-  Livro.criar(livro, (err) => {
+  Livro.criar({
+    titulo: livro.titulo,
+    autor: livro.autor,
+    genero: livro.genero,
+    imagem: livro.imagem,
+    quantidade: 1 // Quantidade inicial, pode ser ajustada conforme necessário
+  }, (err, livroId) => {
     if (err) {
       console.error('Erro ao adicionar livro inicial:', err.message);
+    } else {
+      console.log(`Livro '${livro.titulo}' adicionado com ID ${livroId}`);
     }
   });
 });
